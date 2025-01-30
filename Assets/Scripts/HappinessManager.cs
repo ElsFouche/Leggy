2a46d4d49521adba15aaa450e35a6c6da978cc0c
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-01-29 19:35:00 -0800

    Imported Completed Work and Added Needed Packages

---
+++ Assets/Scripts/HappinessManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HappinessManager : MonoBehaviour
+{
+    //The higher the position, the worse the state
+    public List<AudioClip> emotes;
+    public AudioSource speaker;
+    public TextMeshProUGUI happinessDisplay;
+    public int happinessCount;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            speaker.clip = emotes[0];
+            happinessCount += 100;
+            speaker.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            speaker.clip = emotes[1];
+            happinessCount += 50;
+            speaker.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            speaker.clip = emotes[2];
+            happinessCount -= 50;
+            speaker.Play();
+        }
+
+        happinessDisplay.text = "HAPPINESS: " + happinessCount;
+
+        timer += Time.deltaTime;
+        if (timer >= 1)
+        {
+            happinessCount--;
+            timer = 0;
+        }
+    }
+}
