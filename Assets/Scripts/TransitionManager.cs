c2ab40c490ed1ac29569af8acf5c19013fb3aaf2
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-02-04 15:48:28 -0800

    Attempted Fixes

--- Assets/Scripts/TransitionManager.cs
+++ Assets/Scripts/TransitionManager.cs
@@ -1,30 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TransitionManager : MonoBehaviour
 {
-    public float midScreenMessage;
+    public float fadeInDelay;
+    public float fadeOutDelay;
+    public TMP_Text loreText;
+    //private string loreString;
+
+    bool isTransitioning;
+
+    public Image blackScreen;
+
     // Start is called before the first frame update
     void Start()
     {
+        isTransitioning = false;
 
+        blackScreen.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        loreText.color = new Color(1, 1, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!isTransitioning)
+            {
+                isTransitioning = true;
+                StartCoroutine(fadeToBlack());
+            }
+        }
     }
 
-    private IEnumerator BuildText()
+    public IEnumerator fadeToBlack()
     {
-        for (int i = 0; i < text.Length; i++)
+        while (blackScreen.GetComponent<Image>().color.a < 1.0f)
         {
-            textComponent.text = string.Concat(textComponent.text, text *);
+            blackScreen.GetComponent<Image>().color = new Color
+                (0, 0, 0, blackScreen.GetComponent<Image>().color.a + (Time.deltaTime / Mathf.Abs(2)));
 
-            yield return new WaitForSeconds(timeLapse);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public IEnumerator fadeTextIn()
+    {
+        while (loreText.color.a < 1.0f)
+        {
+            loreText.color = new Color
+                (0, 0, 0, loreText.color.a + (Time.deltaTime / Mathf.Abs(2)));
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public IEnumerator fadeTextOut()
+    {
+        while (loreText.color.a > 0.0f)
+        {
+            loreText.color = new Color
+                (0, 0, 0, loreText.color.a - (Time.deltaTime / Mathf.Abs(2)));
+
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
