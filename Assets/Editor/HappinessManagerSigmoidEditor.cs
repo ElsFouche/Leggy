7ee5cc2202b36ae50264c15e623557366e9a0538
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-03-04 15:32:31 -0800

    Finished Making UI Builder

--- Assets/Editor/HappinessManagerSigmoidEditor.cs
+++ Assets/Editor/HappinessManagerSigmoidEditor.cs
@@ -16,6 +16,19 @@ public class HappinessManagerSigmoidEditor : Editor
 
         visualTree.CloneTree(root);
 
+        var curveField = new CurveField("Sigmoid Curve");
+        //curveField.value = new AnimationCurve(new Keyframe(0, 0, 0, 0), new Keyframe(0.5f, 0.5f, 1, 1),
+         //   new Keyframe(1, 1, 0, 0));
+
+        curveField.RegisterValueChangedCallback(evt =>
+        {
+            var component = target as SigmoidFunction;
+            component.sigmoidCurve = evt.newValue;
+            EditorUtility.SetDirty(component);
+        });
+
+        root.Add(curveField);
+
         return root;
     }
 }
--- Assets/Scripts/SigmoidFunction.cs
+++ Assets/Scripts/SigmoidFunction.cs
@@ -7,6 +7,10 @@ public class SigmoidFunction : MonoBehaviour
     public int minSigmoid;
     public int maxSigmoid;
 
+    public int buffer;
+
+    public AnimationCurve sigmoidCurve;
+
     // Start is called before the first frame update
     void Start()
     {
