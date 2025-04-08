797097251a798f37eea38648f62b8d27f266669c
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-04-08 15:46:07 -0700

    Adjusted Tutorials

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -6,13 +6,20 @@ public class TutorialManager : MonoBehaviour
 {
     public bool moveOn;
     public GameObject blackScreen;
+    public GameObject continueHint;
     float dummy;
+
+    public bool viewingTutorial;
 
     public List<GameObject> sceneObjects;
 
     // Start is called before the first frame update
     void Start()
     {
+        viewingTutorial = true;
+
+        continueHint.transform.localPosition = new Vector3(-625, -100, 0);
+
         //Time.timeScale = 0;
 
         foreach (GameObject sceneObject in FindObjectsOfType<GameObject>())
@@ -22,11 +29,16 @@ public class TutorialManager : MonoBehaviour
 
         for (int i = 0; i < sceneObjects.Count; i++)
         {
+            if (sceneObjects[i].gameObject.name.Contains("Leggy") || sceneObjects[i].gameObject.name == "Wrist_rotate_jnt")
+            {
+                sceneObjects.RemoveAt(i);
+                i--;
+            }
             if (sceneObjects[i].GetComponent<Rigidbody>() != null)
             {
-                sceneObjects[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
-                sceneObjects[i].GetComponent<Rigidbody>().useGravity = false;
-                sceneObjects[i].GetComponent<Rigidbody>().isKinematic = true;
+                //sceneObjects[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+                //sceneObjects[i].GetComponent<Rigidbody>().useGravity = false;
+                //sceneObjects[i].GetComponent<Rigidbody>().isKinematic = true;
             }
         }
         dummy = 0;
@@ -40,6 +52,8 @@ public class TutorialManager : MonoBehaviour
         {
             blackScreen.transform.localPosition = new Vector3(Mathf.SmoothDamp(blackScreen.transform.localPosition.x,
                 0, ref dummy, 0.03f), 0, 0);
+            continueHint.transform.localPosition = new Vector3(Mathf.SmoothDamp(continueHint.transform.localPosition.x,
+                675, ref dummy, 0.03f), Mathf.SmoothDamp(continueHint.transform.localPosition.y, 300, ref dummy, 0.03f), 0);
         }
 
         if (Input.GetKeyDown(KeyCode.JoystickButton0))
@@ -48,18 +62,22 @@ public class TutorialManager : MonoBehaviour
             else if (moveOn)
             {
                 moveOn = false;
+                viewingTutorial = false;
+                blackScreen.SetActive(false);
+
+                gameObject.GetComponent<HappinessManager>().callCoroutine();
 
                 for (int i = 0; i < sceneObjects.Count; i++)
                 {
                     if (sceneObjects[i].GetComponent<Rigidbody>() != null &&
                         sceneObjects[i].GetComponent<Rigidbody>().velocity == Vector3.zero)
                     {
-                        sceneObjects[i].GetComponent<Rigidbody>().velocity = Vector3.one;
-                        sceneObjects[i].GetComponent<Rigidbody>().useGravity = true;
-                        sceneObjects[i].GetComponent<Rigidbody>().isKinematic = false;
+                        //sceneObjects[i].GetComponent<Rigidbody>().velocity = Vector3.one;
+                        //sceneObjects[i].GetComponent<Rigidbody>().useGravity = true;
+                        //sceneObjects[i].GetComponent<Rigidbody>().isKinematic = false;
                     }
                 }
-                blackScreen.SetActive(false);
+                
                 Destroy(this);
             }
         }
