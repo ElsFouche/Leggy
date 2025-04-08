2bc2e5dcf6ee88617a14d7a146d7790d51be2f2c
JustASmoore <Dragondragon.wu21@gmail.com>
2025-04-08 01:27:21 -0700

    Build 4

--- Assets/Scripts/LockedBox.cs
+++ Assets/Scripts/LockedBox.cs
@@ -1,12 +1,36 @@
 using System.Security.Cryptography;
+<<<<<<< Updated upstream
+=======
+using System.Transactions;
+>>>>>>> Stashed changes
 using UnityEngine;
 
 public class LockSystem : MonoBehaviour
 {
     public GameObject lid; // Assign the lid in the Inspector
+<<<<<<< Updated upstream
     public string requiredKeyName = "KeyObject"; // The name of the key object
     public bool isUnlocked = false; // Public bool to track lock state
     public GameObject KeyTransformPosition;
+=======
+    public GameObject dummyLid;
+    public string requiredKeyName = "KeyObject"; // The name of the key object
+    public bool isUnlocked = false; // Public bool to track lock state
+    public GameObject KeyTransformPosition;
+    public GameObject KeyFloatPoint;
+
+    public bool isInternalObject = true;
+    public GameObject InternalGameobject;
+
+    public bool gameEndGoal = false;
+
+
+
+    public TransitionManager transitionManager;
+
+    public GameObject gameover;
+
+>>>>>>> Stashed changes
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,6 +49,10 @@ public class LockSystem : MonoBehaviour
                 BasketParrentTemp.gameObject.transform.position = transform.position;
             }
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
             BasketParrentTemp.rotation = transform.rotation;
             Rigidbody KeyRigid = BasketParrentTemp.GetComponent<Rigidbody>();
             KeyRigid.isKinematic = true;
@@ -38,6 +66,11 @@ public class LockSystem : MonoBehaviour
     {
         if (lid != null)
         {
+<<<<<<< Updated upstream
+=======
+            lid.gameObject.SetActive(true);
+            dummyLid.SetActive(false);
+>>>>>>> Stashed changes
             lid.tag = "Grabbable";
 
             Rigidbody lidRb = lid.GetComponent<Rigidbody>();
@@ -47,8 +80,36 @@ public class LockSystem : MonoBehaviour
                 lidRb.useGravity = true; // Re-enable gravity
             }
 
+<<<<<<< Updated upstream
             isUnlocked = true; // Update lock state
             Debug.Log("Lid Unlocked!");
         }
     }
+=======
+
+            isUnlocked = true; // Update lock state\
+            if (isInternalObject)
+            {
+                InternalGameobject.GetComponent<Rigidbody>().isKinematic = true;
+                InternalGameobject.transform.position = KeyFloatPoint.transform.position;
+            }
+            Debug.Log("Lid Unlocked!");
+
+
+            if (gameEndGoal)
+            {
+                CompleteGoal();
+            }
+        }
+    }
+
+    private void CompleteGoal()
+    {
+        Debug.Log("Goal Completed!");
+        transitionManager.PressToStart();
+
+        // Add any completion logic here (e.g., UI update, level progression, etc.)
+        gameover.SetActive(true);
+    }
+>>>>>>> Stashed changes
 }
