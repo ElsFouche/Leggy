b6a983971ce3a69d88bf65644b71e19358d0a586
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-04-15 16:33:54 -0700

    Finished Bug Fixing Game Over Screen

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GameOverManager : MonoBehaviour
 {
@@ -15,17 +16,23 @@ public class GameOverManager : MonoBehaviour
     public GameObject vignette;
     public GameObject gameOverMenu;
 
+    public EventSystem eventSystem;
+
 
     // Start is called before the first frame update
     void Start()
     {
         happinessManager = FindObjectOfType<HappinessManager>();
+        eventSystem = FindObjectOfType<EventSystem>();
         test = 0f;
 
         fadeBlackTop.GetComponent<Image>().fillAmount = 0;
         fadeBlackBottom.GetComponent<Image>().fillAmount = 0;
         vignette.GetComponent<Image>().color = new Color(1, 0, 0, 0);
         gameOverMenu.SetActive(false);
+
+        eventSystem.GetComponent<EventSystem>().enabled = false;
+        eventSystem.GetComponent<StandaloneInputModule>().enabled = false;
     }
 
 // Update is called once per frame
@@ -42,30 +49,28 @@ void Update()
     public IEnumerator gameOverTransition()
     {
         Debug.Log("Emergency Shutdown");
-        while (Mathf.Approximately(fadeBlackTop.GetComponent<Image>().fillAmount, 1))
+        while (!Mathf.Approximately(fadeBlackTop.GetComponent<Image>().fillAmount, 1f))
         {
             fadeBlackTop.GetComponent<Image>().fillAmount = Mathf.SmoothDamp(fadeBlackTop.GetComponent<Image>().fillAmount,
                 1, ref test, 0.03f);
             fadeBlackBottom.GetComponent<Image>().fillAmount = Mathf.SmoothDamp(fadeBlackBottom.GetComponent<Image>().fillAmount,
                 1, ref test, 0.03f);
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.03f);
         }
 
-        fadeBlackTop.GetComponent<Image>().fillAmount = 1;
-        fadeBlackBottom.GetComponent<Image>().fillAmount = 1;
-
         Debug.Log("I'm seeing red");
-        while (Mathf.Approximately(vignette.GetComponent<Image>().color.a, 0.5f))
+        while (!Mathf.Approximately(vignette.GetComponent<Image>().color.a, 0.5f))
         {
             vignette.GetComponent<Image>().color = new Color(1, 0, 0, Mathf.SmoothDamp(vignette.GetComponent<Image>().color.a,
                 0.5f, ref test, 0.03f));
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.03f);
         }
-        vignette.GetComponent<Image>().color = new Color(1, 0, 0, 0.5f);
 
         yield return new WaitForSeconds(0.25f);
         Debug.Log("Retry?");
         gameOverMenu.SetActive(true);
+        eventSystem.GetComponent<EventSystem>().enabled = true;
+        eventSystem.GetComponent<StandaloneInputModule>().enabled = true;
         yield return null;
 
     }
