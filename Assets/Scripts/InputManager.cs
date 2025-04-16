fb19c99c65a73bb0f6032878d8a29666b782f80c
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-04-15 19:37:53 -0700

    Update InputManager.cs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
+    EventSystem eventSystem;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
+        eventSystem = FindObjectOfType<EventSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (EventSystem.current.currentSelectedGameObject == null)
+            SetFirstSelectedMainMenu();
+    }
+
+    
+    
+    public void SetFirstSelectedMainMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(eventSystem.firstSelectedGameObject);
     }
 }
