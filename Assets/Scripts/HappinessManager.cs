7d954d75e960800c8532a23e07839f1eeefa8e98
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-04-16 19:10:03 -0700

    Finalized Circle Meter

--- Assets/Scripts/HappinessManager.cs
+++ Assets/Scripts/HappinessManager.cs
@@ -43,7 +43,7 @@ public class HappinessManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        viewingTutorial = false;
+        viewingTutorial = true;
         sigmoidFunction = FindObjectOfType<SigmoidFunction>();
 
         backgroundDepressLayer.GetComponent<Image>().fillAmount = backgroundBar.GetComponent<Image>().fillAmount;
